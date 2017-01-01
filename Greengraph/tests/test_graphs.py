6e621942a94f232a072e1358a605e33ebe18e5a8
samstern <samuelostern@gmail.com>
2017-01-01 18:36:17 +0100

    fixed misnamed method in test_graphs

--- Greengraph/tests/test_graphs.py
+++ Greengraph/tests/test_graphs.py
@@ -4,7 +4,9 @@ from mock import Mock
 import yaml
 import os
 
-def test_geolocate():
+def test_graph_construct():
+    #test whether the domain is correct
+    #test whether the start and end strings passed as the correc parameters
     with open(os.path.join(os.path.dirname(__file__),'fixtures','location_pairs.yaml')) as location_pairs:
         pairs=yaml.load(location_pairs)
         for pair in pairs:
@@ -12,6 +14,10 @@ def test_geolocate():
             assert_equal(temp_graph.geocoder.domain, 'maps.google.co.uk')
             assert_equal(temp_graph.start, pair['start'])
             assert_equal(temp_graph.end, pair['end'])
+
+def test_geolocate():
+    pass
+
 def test_location_sequence():
     pass
 def test_green_between():

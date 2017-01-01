71f7d2cf263499770e876982142df5d64c26c7c6
samstern <samuelostern@gmail.com>
2017-01-01 11:13:33 +0100

    added argument shorthands

--- runGreengraph.py
+++ runGreengraph.py
@@ -5,8 +5,8 @@ from argparse import ArgumentParser
 def runIt():
 
     parser = ArgumentParser(description = "Plot the proportion of green pixels in the series of Google Maps satellite images between two locations")
-    parser.add_argument('--start', dest='start', default='New York', help='Where to start, defaults is New York')
-    parser.add_argument('--end', dest='end', default='Chicago', help='Where to end, defaults is Chicago')
+    parser.add_argument('--start', '-s', dest='start', default='New York', help='Where to start, defaults is New York')
+    parser.add_argument('--end', '-e', dest='end', default='Chicago', help='Where to end, defaults is Chicago')
     parser.add_argument('--steps', default=20, help='How many steps between start and end, default is 20')
     arguments=parser.parse_args()
 

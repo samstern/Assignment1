a6d519a23dbfacf192c76697469cf6eec1d15e2d
samstern <samuelostern@gmail.com>
2017-01-01 18:20:23 +0100

    test_geolocate started

---
+++ Greengraph/tests/test_graphs.py
@@ -0,0 +1,18 @@
+from ..greengraph import Greengraph
+from nose.tools import assert_equal
+from mock import Mock
+import yaml
+import os
+
+def test_geolocate():
+    with open(os.path.join(os.path.dirname(__file__),'fixtures','location_pairs.yaml')) as location_pairs:
+        pairs=yaml.load(location_pairs)
+        for pair in pairs:
+            temp_graph=Greengraph(**pair)
+            assert_equal(temp_graph.geocoder.domain, 'maps.google.co.uk')
+            assert_equal(temp_graph.start, pair['start'])
+            assert_equal(temp_graph.end, pair['end'])
+def test_location_sequence():
+    pass
+def test_green_between():
+    pass

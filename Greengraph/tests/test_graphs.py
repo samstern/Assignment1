53a55f231e439cb7ad92d30a11f45f965073274c
samstern <samuelostern@gmail.com>
2017-01-01 21:53:49 +0100

    geolocate test implemented

--- Greengraph/tests/test_graphs.py
+++ Greengraph/tests/test_graphs.py
@@ -1,6 +1,7 @@
 from ..greengraph import Greengraph
-from nose.tools import assert_equal
-from mock import Mock
+import geopy
+from nose.tools import assert_equal, assert_almost_equal
+from mock import Mock, patch
 import yaml
 import os
 
@@ -10,13 +11,23 @@ def test_graph_construct():
     with open(os.path.join(os.path.dirname(__file__),'fixtures','location_pairs.yaml')) as location_pairs:
         pairs=yaml.load(location_pairs)
         for pair in pairs:
-            temp_graph=Greengraph(**pair)
+            temp_graph = Greengraph(**pair)
             assert_equal(temp_graph.geocoder.domain, 'maps.google.co.uk')
             assert_equal(temp_graph.start, pair['start'])
             assert_equal(temp_graph.end, pair['end'])
 
 def test_geolocate():
-    pass
+    #test whether how geolocate is calling geopy
+    with patch.object(geopy.geocoders.GoogleV3,'geocode') as mockgeocode:
+        with open(os.path.join(os.path.dirname(__file__),'fixtures','location_pairs.yaml')) as location_pairs:
+            pairs=yaml.load(location_pairs)
+            for pair in pairs:
+                testGreengraph = Greengraph(**pair)
+                testGreengraph.geolocate(pair['start'])
+                mockgeocode.assert_called_with(pair['start'], exactly_one=False)
+                testGreengraph.geolocate(pair['end'])
+                mockgeocode.assert_called_with(pair['end'], exactly_one=False)
+
 
 def test_location_sequence():
     pass

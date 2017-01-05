d21e5bacb7610d5df1d9bc68e5ff188775dfb7a5
samstern <samuelostern@gmail.com>
2017-01-05 12:59:49 +0100

    test green_between added

--- Greengraph/tests/test_graphs.py
+++ Greengraph/tests/test_graphs.py
@@ -1,12 +1,14 @@
 from ..greengraph import Greengraph
+from ..map import Map
 import geopy
 from nose.tools import assert_equal, assert_almost_equal
 import numpy.testing as np_test
 from mock import Mock, patch
+from itertools import cycle
 import yaml
 import os
 
-def test_graph_construct():
+def test_graph_constructor():
     #test whether the domain is correct
     #test whether the start and end strings passed as the correc parameters
     with open(os.path.join(os.path.dirname(__file__),'fixtures','location_pairs.yaml')) as location_pairs:
@@ -31,13 +33,20 @@ def test_geolocate():
 
 
 def test_location_sequence():
-    sequence=Greengraph.location_sequence(Greengraph("New York","Chicago"), (0,0),(20,20),5)
-    np_test.assert_equal(sequence[0],(0,0))
-    np_test.assert_equal(sequence[1],(5.0,5.0))
-    np_test.assert_equal(sequence[2],(10.0,10.0))
-    np_test.assert_equal(sequence[-1],(20.0,20.0))
-
-
+    mock_image= open(os.path.join(os.path.dirname(__file__),'fixtures','NY_2.png'),'rb')
+    with patch('requests.get', return_value=Mock(content=mock_image.read())) as mock_get:
+        sequence=Greengraph.location_sequence(Greengraph("New York","Chicago"), (0,0),(20,20),5)
+        np_test.assert_equal(sequence[0],(0,0))
+        np_test.assert_equal(sequence[1],(5.0,5.0))
+        np_test.assert_equal(sequence[2],(10.0,10.0))
+        np_test.assert_equal(sequence[-1],(20.0,20.0))
 
 def test_green_between():
-    pass
+    mock_map = open(os.path.join(os.path.dirname(__file__),'fixtures','NY_2.png'),'rb')
+    mock_geolocate = Mock(name="geolocate", side_effect=cycle([range(10)]))
+    steps = 20
+    with patch('requests.get', return_value=Mock(content=mock_map.read())) as mock_get:
+        with patch.object(Greengraph,'geolocate',mock_geolocate) as mockgeolocate:
+            test_Greengraph = Greengraph('New York','Chicago')
+            assert test_Greengraph.green_between(steps) == ([28125]*steps)
+
